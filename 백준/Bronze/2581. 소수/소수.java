47d0729f1b140f6d5b2fe77c449cf8f207facad1
moomint8 <112872644+moomint8@users.noreply.github.com>
2024-01-16 15:48:52 +0900

    [Bronze II] Title: 소수, Time: 148 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        int min = 0;
+        int sum = 0;
+
+        boolean[] prime = new boolean[n + 1];
+
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i < (int)Math.sqrt(n) + 1; i++) {
+            if(prime[i]) { continue; }
+
+            for (int j = i + i; j <= n; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (!prime[i]) {
+                sum += i;
+                if (min == 0) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            writer.write(-1 + "\n");
+        } else {
+            writer.write(sum + "\n" + min + "\n");
+        }
+
+        writer.flush();
+        writer.close();
+    }
+}
