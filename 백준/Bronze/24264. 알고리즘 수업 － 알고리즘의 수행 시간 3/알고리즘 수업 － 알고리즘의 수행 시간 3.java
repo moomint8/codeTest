e6239f26ecdb623c3118d14f23bc103ba666730d
moomint8 <112872644+moomint8@users.noreply.github.com>
2024-01-16 16:10:57 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 140 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long num = Integer.parseInt(reader.readLine());
+        num *= num;
+
+        writer.write(num + "\n2");
+
+        writer.flush();
+        writer.close();
+    }
+}
