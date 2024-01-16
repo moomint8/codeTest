00e57d9f69d50e7410e7e4fbf50d41d7807981a7
moomint8 <112872644+moomint8@users.noreply.github.com>
2024-01-16 14:07:07 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        double c = a / b;
+        System.out.println(c);
+    }
+}
