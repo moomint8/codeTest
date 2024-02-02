0674e9f080b620bbd832c0b15b61cfd4318b2cc0
moomint8 <112872644+moomint8@users.noreply.github.com>
2024-02-02 10:11:01 +0900

    [Silver III] Title: 파도반 수열, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,37 +1,24 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long[] arr = new long[101];
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 2;
+        arr[5] = 2;
+        for (int j = 6; j < 101; j++) {
+            arr[j] = arr[j - 1] + arr[j - 5];
+        }
+
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int m = Integer.parseInt(br.readLine());
-            bw.write(getResult(m) + "\n");
+            System.out.println(arr[m]);
         }
-        bw.flush();
-        bw.close();
-    }
-
-    private static long getResult(int m) {
-        if (m > 5) {
-            long[] arr = new long[m + 1];
-            arr[1] = 1;
-            arr[2] = 1;
-            arr[3] = 1;
-            arr[4] = 2;
-            arr[5] = 2;
-            for (int j = 6; j < m + 1; j++) {
-                arr[j] = arr[j - 1] + arr[j - 5];
-            }
-            return arr[m];
-        }
-        if (m == 1) return 1;
-        if (m == 2) return 1;
-        if (m == 3) return 1;
-        if (m == 4) return 2;
-        if (m == 5) return 2;
-
-        return 0;
     }
 }
