cdf5b76762b45418ad475b8b6f70da5340e2dba3
moomint8 <112872644+moomint8@users.noreply.github.com>
2024-10-16 03:29:09 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        k = k - (n / 10);
-        
-        if(k > 0) return (n * 12000) + (k * 2000);
-        
-        return n * 12000;
+        return (n * 12000) + ((k - (n / 10)) * 2000);
     }
 }
