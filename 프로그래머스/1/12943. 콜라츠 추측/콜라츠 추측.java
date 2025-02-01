5578a42baf1ddb6e5a0be497086197a9cbbb9905
moomint8 <112872644+moomint8@users.noreply.github.com>
2025-02-01 11:25:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,20 +1,13 @@
 class Solution {
     public int solution(int origin) {
         
-        int answer = 0;
         long num = origin;
         
         for(int i = 0; i < 500; i++) {
             
-            if(num == 1) return answer;
+            if(num == 1) return i;
             
-            answer++;
-            
-            if(num % 2 == 0) {
-                num /= 2;
-                continue;
-            }
-            num = (num * 3) + 1;
+            num = (num % 2 == 0) ? num / 2 : (num * 3) + 1;
         }
         
         return -1;
